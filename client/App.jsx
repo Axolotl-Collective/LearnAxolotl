8aea66fb872e7805663438940d53858b137f7685
Tim Lee <tl12@DESKTOP-F1V1URD>
2023-01-14 11:09:36 -0800

    completed structure of navbar and routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import HomePage from "./components/HomePage.jsx";
+import Login from "./components/Login.jsx";
+import MainGame from "./components/MainGame.jsx";
 
 const App = () => {
   return(
-    <div>
-      Hello
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path ='/login' element={<Login />}></Route>
+        <Route path ='/game' element={<MainGame />}></Route>
+        <Route path='/' element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 };
 
