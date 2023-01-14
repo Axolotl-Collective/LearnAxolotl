3599c59355a717a2a539b36b6cee34eaa31b57c2
brett-webster <brettwebster@hotmail.com>
2023-01-13 23:45:59 -0500

    set up functional shells for server.js, index.html, index.js and App.jsx to test webpack -- basic HTML rendering successfully to localhost3000 on npm start

---
+++ client/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const App = (props) => {
+  // render() {
+  return <div>Success!</div>;
+};
+// }
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import App from ".client/App.jsx";
+
+render(<App />, document.getElementById("root"));
