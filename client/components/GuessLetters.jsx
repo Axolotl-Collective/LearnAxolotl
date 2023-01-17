fb449ef877ed127b0e531d00c4bb4b18f36c393c
Tim Lee <tl12@DESKTOP-F1V1URD>
2023-01-16 21:50:25 -0800

    preparing to pull and merge dev

--- client/components/GuessLetters.jsx
+++ client/components/GuessLetters.jsx
@@ -1,16 +1,35 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 const GuessLetters = (props) => {
   const { guessLetter, guessed } = props;
   console.log(guessed)
-  console.log(guessLetter);
 
   const letterInput = useRef(null);
+
+  const checkInput = () => {
+    const letter = letterInput.current.value.toUpperCase();
+    console.log(letter)
+
+    if(letter.length > 1) throw new Error('must be single character')
+
+    return guessLetter(letter)
+  }
+
+  const handleKeyDown = event => {
+    if(event.key === 'Enter') {
+      return checkInput();
+    }
+  }
+
+  useEffect(() => {
+    
+    letterInput.current.value = '';
+  })
 
   return(
     <div>
       <input ref={letterInput} type='text' id='input-field' placeholder="Guess a letter"></input>
-      <button onClick={() => guessLetter(letterInput.current.value)}>Submit</button>
+      <button onClick={checkInput} onKeyDown={handleKeyDown}>Submit</button>
     </div>
   )
 };
